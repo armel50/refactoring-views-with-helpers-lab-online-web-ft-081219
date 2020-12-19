7e8c5e115f054357f134c63047d92cf4ffe53e25
Cloe Kouadjo <armelfli50@gmail.com>
2020-12-19 16:01:23 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,4 +1,5 @@
 module ArtistsHelper
+<<<<<<< HEAD
     def display_artist(song)
         if song.artist 
           link_to(song.artist.name, artist_path(song.artist))
@@ -6,4 +7,13 @@ module ArtistsHelper
           link_to("Add Artist", edit_song_path(song))
         end
        end
+=======
+  def display_artist(song)
+   if song.artist 
+     link_to(song.artist.name, artist_path(song.artist))
+   else 
+     link_to("Add Artist", edit_song_path(song))
+   end
+  end
+>>>>>>> 222a50fb2b33e16795f28a478a051dcc10e8ad23
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,11 @@ class Song < ActiveRecord::Base
 
   def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
+<<<<<<< HEAD
     self.artist = artist
+=======
+    p artist 
+    p self
+>>>>>>> 222a50fb2b33e16795f28a478a051dcc10e8ad23
   end
 end
